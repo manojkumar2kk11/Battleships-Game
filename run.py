11fad1c98f878a47081e899698bbf1ced8716dea
Manojkumar Ayyappan <manojkumar2kk11@gmail.com>
2024-09-10 22:20:44 +0000

    Add a class battleship and add the initiase method for the batteship

--- run.py
+++ run.py
@@ -13,5 +13,18 @@ class Board:
         self.ships = []
         self.guesses = set() 
 
-
-
+# Create a class battleship Game
+class BattleshipGame:
+    def __init__(self):
+        """
+        Initialise a new instance for the class battleship Game
+        Get the size of board from user,create board for player and computer, Number of ships in the board based on the board size.
+        Hits, attempts and maximum number of attempts can be multiple by the size of the board
+        """
+        self.size = self.get_board_size()
+        self.player_board = Board(self.size)  
+        self.computer_board = Board(self.size, computer=True)  
+        self.num_ships = self.size 
+        self.hits = 0
+        self.attempts= 0
+        self.max_attempts = self.size * 2  
