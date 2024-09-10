9cb2c59f1fa3e099aabbf4db435ef539120b6fb6
Manojkumar Ayyappan <manojkumar2kk11@gmail.com>
2024-09-10 21:58:28 +0000

    Add a class board and add the initialise function for class board

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Create a class Board 
+class Board:
+    def __init__(self, size, computer=False):
+        """
+        Initialise a new instance for the class board
+        Size of board, computer, create default doller symbol for the size of the board.
+        Ships and Guesses
+        """
+        self.size = size
+        self.computer = computer
+        self.board = [["$"] * size for _ in range(size)]
+        self.ships = []
+        self.guesses = set() 
+
+
+
